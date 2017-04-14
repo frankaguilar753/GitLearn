144ef1924dcc7aa6a1eb29730f26de31bcc63554
“koyomiararagi” <franko753_95@hotmail.com>
2017-04-14 14:21:56 -0500

    mostrar-array

--- cc.cpp
+++ cc.cpp
@@ -11,10 +11,12 @@ int main(){
 		cout<<a;
 	}
 	cout<<"iteracion";
-	char b[4];
+	char c[4];
 	b={'1','f','c','3'};
 	cout<<b;
-	
+	for (int b=0;b<4;b++){
+		cout<<c[b]
+	}
 	system("pasuse");
 	return 0;
 }
